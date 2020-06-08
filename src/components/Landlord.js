e7087265564b69ec0ba2a4513e94474df69eb995
haphamo <hapham0392@gmail.com>
2020-06-08 16:29:52 -0400

    Created state for the display, prevent default for the button

--- src/components/Landlord.js
+++ src/components/Landlord.js
@@ -1,26 +1,39 @@
 import React, { useState } from "react";
 
+
 function Landlord() {
   const [landlord, setLandlord] = useState("");
+  const [displayLandlord, setDisplayLandlord] = useState("");
   // prevent default on form submission
   const handleChange = evt => {
-    // evt.preventDefault()
     setLandlord(evt.target.value)
-    console.log(landlord)
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setDisplayLandlord(landlord)
+  }
+
+  const style = {
+    // display: 'flex',
+    // flexDirection: 'column'
   }
 
   return (
-    <form className="add-landlord">
-      <label name="landlord">
-        Please add a landlord:
-        <input
-          type="text"
-          onChange={handleChange}
-          name="landlord"
-        ></input>
-        <button type="submit">Add Landlord</button>
-      </label>
-    </form>
+    <section className="container">
+      <form className="add-landlord" onSubmit={handleSubmit}>
+        <label name="landlord" style={style}>
+          Please add a landlord:
+          <input
+            type="text"
+            onChange={handleChange}
+            name="landlord"
+          ></input>
+          <button type="submit">Add Landlord</button>
+        </label>
+      </form>
+      The landlord's name is: {displayLandlord}
+    </section>
   );
 }
 
