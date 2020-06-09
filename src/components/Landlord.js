dbe5e33636992d854992cc1895cb98eccf4c9f5a
haphamo <hapham0392@gmail.com>
2020-06-09 15:20:05 -0400

    Added onChange and value attr to select component of landlord, changes are functional

--- src/components/Landlord.js
+++ src/components/Landlord.js
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-function Landlord() {
 
+const landlords = {};
 
-
-  const rentReceipts = {
-    1: {
-      id: 1,
-      landlordName: 'Andrew Ly',
-      tenantName: 'Sebastian Gustavo',
-      dateReceived: '06/01/2020',
-      propertyAddress: '33 Bremnar Avenue',
-      rentAmount: 240000,
-    }
+// fixture rent receipt
+const rentReceipts = {
+  1: {
+    id: 1,
+    landlordName: 'Andrew Ly',
+    tenantName: 'Sebastian Gustavo',
+    dateReceived: '06/01/2020',
+    propertyAddress: '33 Bremnar Avenue',
+    rentAmount: 240000,
   }
+}
+
+function Landlord() {
+  const [landlord, setLandlord] = useState('')
+
+  const handleLandlordChange = (evt) => {
+    setLandlord(evt.target.value)
+  }
+  
 
   return (
     <section className="container">
-  
 
     <hr></hr>
       <label for="landlord-select">Choose a landlord:
       </label>
-      <select name="landlords" id="landlord-select">
+      <select name="landlords" id="landlord-select" value={landlord} onChange={handleLandlordChange}>
         <option value="">--Please choose a landlord--</option>
         <option value='Andrew Ly'>Andrew Ly</option>
       </select>
@@ -37,7 +44,7 @@ function Landlord() {
       <hr></hr>
       <main className="sample-receipt" style={{display: 'flex', flexDirection: 'column'}}>
         <time>Date: {Date()}</time>
-        <text>Landlord: {rentReceipts['1'].landlordName}</text>
+        <text>Landlord: {landlord}</text>
         <text>Tenant: {rentReceipts['1'].tenantName}</text>
      
         <address>Address: {rentReceipts['1'].propertyAddress}</address>
