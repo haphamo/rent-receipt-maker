5f6595e7f9d0d90f80206ec6c740f6503600393a
haphamo <hapham0392@gmail.com>
2020-06-11 22:25:36 -0400

    Adding a new landlord to the landlord list is functional

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { landlords } from '../fixture/fixture.js';
 
 function CreateProfile() {
-  // const displayAllLandlords = () => {
-  //   return
-  // }
+  const [newLandlord, setNewLandlord] = useState('');
+  const [allLandlords, setAllLandlords] = useState(landlords)
+
+  const handleSubmitNewLandlord = (evt) => {
+    evt.preventDefault();
+    const newLandlordEntry = {id: uuidv4(), name: newLandlord}
+    setAllLandlords([...allLandlords, newLandlordEntry])
+  }
+  
+  const handleNewLandlord = (evt) => {
+    setNewLandlord(evt.target.value)
+  }
+
+  const displayAllLandlords = (data) => {
+    const result = data.map(landlord => {
+      return(
+        <li key={landlord.id}>{landlord.name}</li>
+      )
+    })
+    return result
+  }
+  
+
   return(
     <section>
-    <label>
-      <input name="name" type="text" placeholder="John Smith"></input>
-    </label>
-    <button>Add A Landlord</button>
+      <form onSubmit={handleSubmitNewLandlord}>
+        <label htmlFor="landlord-name">
+          <input id="landlord-name" name="name" value={newLandlord} onChange={handleNewLandlord}type="text" placeholder="John Smith"></input>
+          <button type="submit">Add A Landlord</button>
+        </label>
+
+      </form>
     <ul>
-      <li>Andrew Ly</li>
-      <li>Ha Pham</li>
-      <li>Tina Pham</li>
+      {displayAllLandlords(allLandlords)}
     </ul>
 
     </section>
