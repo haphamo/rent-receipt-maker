a0633d66a4cf624f3e20a6b5a58346edbe58a736
haphamo <hapham0392@gmail.com>
2020-06-15 15:36:23 -0400

    Added input field for rent amount and select options for payment method

--- src/components/CreateTenant.js
+++ src/components/CreateTenant.js
@@ -5,14 +5,28 @@ import { allTenants } from '../fixture/fixture.js';
 function CreateTenant({ allAddressesForSelect }) {
   const [newTenant, setNewTenant] = useState('')
   const [addressOfTenant, setAddressOfTenant] = useState('')
+  const [rent, setRent] = useState('')
+  const [paymentMethod, setPaymentMethod] = useState('etransfer')
 
   const handleNewTenant = evt => {
     setNewTenant(evt.target.value)
   }
 
+  const handleAddressOfTenant = evt => {
+    setAddressOfTenant(evt.target.value)
+  }
+
+  const handleRentChange = evt => {
+    setRent(evt.target.value)
+  }
+
+  const handlePaymentMethodChange = evt => {
+    setPaymentMethod(evt.target.value)
+  }
+
   const handleSubmit = evt => {
     evt.preventDefault();
-    alert('submitted!')
+    // alert(`New Tenant: ${newTenant}, Address: ${addressOfTenant}, Rent: ${rent}, Payment Method: ${paymentMethod}`)
   }
 
   return(
@@ -23,10 +37,21 @@ function CreateTenant({ allAddressesForSelect }) {
           <input id="tenant-name" name="name" value={newTenant} onChange={handleNewTenant} type="text" placeholder="Tenant Name" required></input>
         </label>
 
-        <label htmlFor="which-landlord">
-          <select>
-            <option value="">Which property?</option>
+        <label htmlFor="property-address">
+          <select name="property-address" id="property-address" value={addressOfTenant} onChange={handleAddressOfTenant}>
+            <option value="" disabled>Which property?</option>
             {allAddressesForSelect}
+          </select>
+        </label>
+
+        <label htmlFor="rent-amount">
+          $<input id="rent-amount" type="number" placeholder="500" min="1" value={rent} onChange={handleRentChange} required></input>
+        </label>
+
+        <label>
+          <select name="payment-method" id="payment-method" value={paymentMethod} onChange={handlePaymentMethodChange}>
+            <option value="etransfer">E-transfer</option>
+            <option value="cash">Cash</option>
           </select>
         </label>
         <button type="submit">Add Tenant</button>
