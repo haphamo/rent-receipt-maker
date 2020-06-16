beb5b9e0aa4aed2c98ff46b5dc51b0b2b7fd3425
haphamo <hapham0392@gmail.com>
2020-06-15 21:45:08 -0400

    Added landlord key for allTenants

--- src/fixture/fixture.js
+++ src/fixture/fixture.js
@@ -68,24 +68,28 @@ const allTenants = {
       id: 111,
       rentAmount: 240000,
       address: '9 Spadina Avenue',
+      landlord: 'Andrew Ly',
       paymentMethod: 'e-transfer'
   },
   'Katerina': {
       id: 222,
       rentAmount: 200000,
       address: '188 Bremnar Avenue',
+      landlord: 'Andrew Ly',
       paymentMethod: 'cash '
   },
   'Rob Long': {
       id: 333,
       rentAmount: 50000,
       address: '300 Harvie Road',
+      landlord: 'Ha Pham',
       paymentMethod: 'e-transfer'
   },
   'Senorita': {
       id: 444,
       rentAmount: 210000,
       address: '200 Highway 7',
+      landlord: 'Ha Pham',
       paymentMethod: 'e-transfer'
   }
 }
