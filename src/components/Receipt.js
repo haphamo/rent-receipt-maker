2844067a11e3e35005ee2785fabb1db88c29d733
haphamo <hapham0392@gmail.com>
2020-09-24 10:50:50 -0400

    Added memo field for the receipt, added form wrapper and created func to handle submit with alert

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -6,6 +6,7 @@ function Landlord({ fixture, setFixture }) {
   const [landlord, setLandlord] = useState("");
   const [tenant, setTenant] = useState("");
   const [paymentMethod, setPaymentMethod] = useState("");
+  const [memo, setMemo] = useState("");
 
   const handleLandlordChange = (evt) => {
     setLandlord(evt.target.value);
@@ -14,11 +15,15 @@ function Landlord({ fixture, setFixture }) {
 
   const handleTenantChange = (evt) => {
     setTenant(evt.target.value);
-    setPaymentMethod(tenants[evt.target.value].paymentMethod)
+    setPaymentMethod(tenants[evt.target.value].paymentMethod);
   };
 
   const handlePaymentMethod = (evt) => {
     setPaymentMethod(evt.target.value);
+  };
+
+  const handleMemoChange = (evt) => {
+    setMemo(evt.target.value);
   };
 
   const getAllLandlords = Object.entries(landlords);
@@ -51,7 +56,7 @@ function Landlord({ fixture, setFixture }) {
         </Fragment>
       );
     }
-    
+
     return (
       <option value="">
         --No tenants for this landlord, please create one!--
@@ -59,65 +64,84 @@ function Landlord({ fixture, setFixture }) {
     );
   };
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert('Submitted!')
+  };
+
   const getReceipts = Object.entries(receipts);
-  console.log(getReceipts[0][1])
+
   return (
-    <section className="container">
+    <section className="container" style={{ margin: "1em" }}>
       <hr></hr>
-      <label htmlFor="landlord-select">Choose a landlord:</label>
-      <select
-        name="landlords"
-        id="landlord-select"
-        value={landlord}
-        onChange={handleLandlordChange}
-      >
-        <option value="" disabled>
-          --Please choose a landlord--
-        </option>
-        {allLandlordsForSelect}
-      </select>
-      {landlord && (
-        <section className="tenant-select">
-          <label htmlFor="tenant-select">Choose a tenant:</label>
-          <select
-            name="tenants"
-            id="tenant-select"
-            value={tenant}
-            onChange={handleTenantChange}
-          >
-            {displayTenants(landlord)}
-          </select>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="landlord-select">Choose a landlord:</label>
+        <select
+          required
+          name="landlords"
+          id="landlord-select"
+          value={landlord}
+          onChange={handleLandlordChange}
+        >
+          <option value="" disabled>
+            --Please choose a landlord--
+          </option>
+          {allLandlordsForSelect}
+        </select>
+        {landlord && (
+          <section className="tenant-select">
+            <label htmlFor="tenant-select">Choose a tenant:</label>
+            <select
+              required
+              name="tenants"
+              id="tenant-select"
+              value={tenant}
+              onChange={handleTenantChange}
+            >
+              {displayTenants(landlord)}
+            </select>
+          </section>
+        )}
+        <hr></hr>
+        <section className="create-receipt" style={{ display: "inline-grid" }}>
+          <section>Landlord: {landlord}</section>
+          <section>Tenant: {tenant}</section>
+
+          <address>Address: {tenant && tenants[tenant].address}</address>
+          <data className="money">
+            Rent Amount: ${tenant && tenants[tenant].rentAmount / 100}
+          </data>
+
+          <label htmlFor="payment-method">
+            Payment Method:
+            <select
+              id="payment-method"
+              name="payment-method"
+              value={paymentMethod}
+              onChange={handlePaymentMethod}
+            >
+              <option value="" disabled>
+                --Please select--
+              </option>
+              <option value="cash">Cash</option>
+              <option value="e-transfer">E-transfer</option>
+            </select>
+          </label>
+          <label>
+            Memo:
+            <input
+              placeholder="optional"
+              type="text"
+              value={memo}
+              onChange={handleMemoChange}
+            />
+          </label>
+          {/* Submit action on the button */}
+          <button type="submit">Create Rent Receipt!</button>
         </section>
-      )}
+      </form>
+
       <hr></hr>
-      <main className="create-receipt" style={{ display: "inline-grid" }}>
-        <section>Landlord: {landlord}</section>
-        <section>Tenant: {tenant}</section>
-
-        <address>Address: {tenant && tenants[tenant].address}</address>
-        <data className="money">
-          Rent Amount: ${tenant && tenants[tenant].rentAmount / 100}
-        </data>
-
-        <label htmlFor="payment-method">
-          Payment Method:
-          <select
-            id="payment-method"
-            name="payment-method"
-            value={paymentMethod}
-            onChange={handlePaymentMethod}
-          >
-            <option value="" disabled>
-              --Please select--
-            </option>
-            <option value="cash">Cash</option>
-            <option value="e-transfer">E-transfer</option>
- 
-          </select>
-        </label>
-        {/* Submit action on the button */}
-        <button>Create Rent Receipt!</button>
-      </main>
       <section className="all-receipts">
         <section key={getReceipts[0][1].id}>
           <section>Landlord: {getReceipts[0][1].landlord}</section>
@@ -125,7 +149,7 @@ function Landlord({ fixture, setFixture }) {
           <section>Address: {getReceipts[0][1].property}</section>
           <section>Amount: ${getReceipts[0][1].rentAmount / 100}</section>
           <section>Date Received {getReceipts[0][1].dateCreated}</section>
-          <section>Notes: {getReceipts[0][1].notes}</section>
+          <section>Memo: {getReceipts[0][1].notes}</section>
         </section>
       </section>
     </section>
