296181900670601a614983810b050228680ce19f
haphamo <hapham0392@gmail.com>
2020-10-01 14:53:53 -0400

    imported stylesheet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './App.css'
 import { Router, Link } from "@reach/router";
 import Receipt from "./components/Receipt.js";
 import Home from "./components/Home.js";
